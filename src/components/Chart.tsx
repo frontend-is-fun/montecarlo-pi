86e6ffae1382a0d5a5f7f272c72c7dda0ce9af18
feiandxs <yiziyint@gmail.com>
2024-04-24 11:47:36 +0800

    component refactor

---
+++ src/components/Chart.tsx
@@ -0,0 +1,30 @@
+import { Line } from 'react-chartjs-2';
+import 'chart.js/auto';
+
+export interface ChartComponentProps {
+  piHistory: number[];
+}
+
+const ChartComponent = (props: ChartComponentProps) => {
+  const { piHistory } = props;
+  return (
+    <div className='col-span-1 chart-container'>
+      <Line
+        data={{
+          labels: new Array(piHistory.length).fill('').map((_, index) => `#${piHistory.length - index}`),
+          datasets: [
+            {
+              label: 'Pi Estimate',
+              data: piHistory,
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1,
+            },
+          ],
+        }}
+      />
+    </div>
+  );
+};
+
+export default ChartComponent;
---
+++ src/components/ControlPanel.tsx
@@ -0,0 +1,125 @@
+import { FormattedMessage } from 'react-intl';
+
+export interface ControlPanelProps {
+  totalSteps: number;
+  setTotalSteps: (value: number) => void;
+  squareLength: number;
+  setSquareLength: (value: number) => void;
+  handleStartStop: () => void;
+  isCalculating: boolean;
+  isPaused: boolean;
+  piEstimate: number;
+  currentStep: number;
+  reset: () => void;
+}
+
+const ControlPanel = (props: ControlPanelProps) => {
+  const {
+    totalSteps,
+    setTotalSteps,
+    squareLength,
+    setSquareLength,
+    isCalculating,
+    isPaused,
+    handleStartStop,
+    piEstimate,
+    currentStep,
+    reset,
+  } = props;
+
+  return (
+    <div className='flex flex-col justify-start items-center w-[600px] mt-8 border shadow-xl rounded-md py-8 px-4 font-bold'>
+      <div className='grid w-full grid-cols-2 gap-4'>
+        {/* total steps */}
+        <div className='flex flex-row items-center justify-start'>
+          <FormattedMessage
+            id='total_steps'
+            defaultMessage='Total Steps'
+          />
+          <input
+            id='totalStepsInput'
+            type='number'
+            value={totalSteps}
+            onChange={(e) => setTotalSteps(Math.max(1, parseInt(e.target.value, 10)))}
+            disabled={isCalculating || isPaused}
+            className='w-32 max-w-xs ml-4 input input-bordered'
+          />
+        </div>
+        {/* side length */}
+        <div className='flex flex-row items-center justify-start'>
+          <FormattedMessage
+            id='side_length'
+            defaultMessage='Side Length of Square'
+            aria-label='Side Length of Square'
+          />
+          <input
+            id='squareLengthInput'
+            type='number'
+            value={squareLength}
+            onChange={(e) => setSquareLength(parseInt(e.target.value, 10))}
+            disabled={isCalculating || isPaused}
+            className='w-32 max-w-xs ml-4 input input-bordered'
+          />
+        </div>
+        {/* current step */}
+        <div className='flex flex-row items-center justify-start'>
+          <FormattedMessage
+            id='current_step'
+            defaultMessage='Current Step'
+            aria-label='Current Step'
+          />
+          <span className='ml-4 font-black text-orange-500'>
+            {currentStep}
+          </span>
+        </div>
+        {/* result */}
+        <div>
+          <FormattedMessage
+            id='result'
+            defaultMessage='Result'
+            aria-label='Result'
+          />
+          <span className='ml-4 font-black text-green-700'>
+            {piEstimate}
+          </span>
+        </div>
+      </div>
+
+      <div className='flex flex-row items-center mt-4 justify-cente'>
+
+        <button
+          type='button'
+          onClick={handleStartStop}
+          className='btn btn-neutral'
+        >
+          {isCalculating && !isPaused ? (
+            <FormattedMessage
+              id='pause'
+              defaultMessage='Pause'
+            />
+          ) : (
+            <FormattedMessage
+              id='start'
+              defaultMessage='Start'
+            />
+          )}
+        </button>
+        <button
+          type='button'
+          onClick={reset}
+          aria-label='Reset'
+          className='ml-12 btn btn-info'
+        >
+          <FormattedMessage
+            id='reset'
+            defaultMessage='Reset'
+          />
+
+        </button>
+      </div>
+
+    </div>
+  );
+};
+
+export default ControlPanel;
